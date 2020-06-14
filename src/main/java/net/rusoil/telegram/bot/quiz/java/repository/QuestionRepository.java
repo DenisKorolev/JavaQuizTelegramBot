cf868dc696ee8b21a41cf12a7463972849ed62fa
DenisKorolev <valveufa@gmail.com>
2020-06-14 20:21:48 +0300

    Repository public endpoints were disabled

--- src/main/java/net/rusoil/telegram/bot/quiz/java/repository/QuestionRepository.java
+++ src/main/java/net/rusoil/telegram/bot/quiz/java/repository/QuestionRepository.java
@@ -2,6 +2,8 @@ package net.rusoil.telegram.bot.quiz.java.repository;
 
 import net.rusoil.telegram.bot.quiz.java.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
